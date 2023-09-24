2084b111a8926fede160fbb17bc2b15089ac7370
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-09-24 16:12:36 -0400

    demo

--- Course_Correction.py
+++ Course_Correction.py
@@ -1,1 +1,4 @@
-# This file contains the functions that wil be used to select the correct the course of DORM-E, collisions and 
+# This file contains the functions that wil be used to select the correct the course of DORM-E, collisions and veering
+
+if __name__ == "__main__":
+    print("Hello, World!")
--- Path_Selection.py
+++ Path_Selection.py
@@ -1,3 +1,6 @@
 # This file contains the functions that wil be used to select the proper path
 
 import Map_Database.py as md
+
+if __name__ == "__main__":
+    print("Hello, World!")
