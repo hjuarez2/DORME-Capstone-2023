091cc9d8aebafa59a9f022280d4939752bc5755b
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-10-24 17:51:45 -0400

    Update motor_controller_code.py

--- motor_controller_code.py
+++ motor_controller_code.py
@@ -5,9 +5,9 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD)
 
 # Motor Pins: replace with the GPIO pins you're using
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
+Motor1A = 18
+Motor1B = 23
+Motor1E = 24
 
 # Setup GPIO pins
 GPIO.setup(Motor1A, GPIO.OUT)
