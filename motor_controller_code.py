16e744a887be69dffc8af22dad525e015d21b540
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-10-29 21:00:31 -0400

    Update motor_controller_code.py

--- motor_controller_code.py
+++ motor_controller_code.py
@@ -21,7 +21,9 @@ p.start(25)
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
+print("\n")
+x='r'
+x='l'
 
 while(1):
 
