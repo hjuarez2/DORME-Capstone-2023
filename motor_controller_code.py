10a694ab50077949872a44b1fc86113bdb7c4c94
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-10-31 16:11:48 -0400

    Update motor_controller_code.py

--- motor_controller_code.py
+++ motor_controller_code.py
@@ -15,10 +15,17 @@ temp1=1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+
 GPIO.setup(ena,GPIO.OUT)
+GPIO.setup(enb,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
 p=GPIO.PWM(ena,1000)
+p=GPIO.PWM(enb,1000)
 
 p.start(25)
 print("\n")
@@ -36,6 +43,8 @@ while(1):
         if(temp1==1):
          GPIO.output(in1,GPIO.HIGH)
          GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.HIGH)
+         GPIO.output(in4,GPIO.LOW)
          print("forward")
          x='z'
         else:
