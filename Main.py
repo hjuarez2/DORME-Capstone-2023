5585dbdbc2fe8f670cd8e0f114ed7d946034a4da
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-11-02 16:35:01 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,15 +4,19 @@ import Course_Correction.py as cc
 import Path_Selection.py as ps
 import sensors as ss
 import draft_pathfind.py as pf
+import motor_controller_code_function.py mc
 
 if __name__ == "__main__":
 
-    n = len(sys.argv)
-    
-    path = a_star(sys.argv[1], sys.argv[2], sys.argv[3])
-    currentNode=sys.argv[1]
+    forward()
 
-    for i in range(0,len(path)):
-        driveto(currentNode,path[i])
     
+    #n = len(sys.argv)
+    
+    #path = a_star(sys.argv[1], sys.argv[2], sys.argv[3])
+    #currentNode=sys.argv[1]
+
+    #for i in range(0,len(path)):
+        #driveto(currentNode,path[i])
+
 
