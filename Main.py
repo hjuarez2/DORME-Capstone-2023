9e7793e6ac5b0df7ea16e5637f07898077d9e1e0
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-11-02 16:39:56 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,79 @@
 #import Path_Selection.py as ps
 #import sensors as ss
 #import draft_pathfind.py as pf
-import motor_controller_code_function.py as mc
+package import RPi.GPIO as GPIO          
+from time import sleep
+#import motor_controller_code_function.py as mc
+
+# Initialization
+in1 = 24
+in2 = 23
+in3 = 17
+in4 = 27
+ena = 12
+enb = 13
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(ena,GPIO.OUT)
+GPIO.setup(enb,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+p1 = GPIO.PWM(ena,1000)
+p2 = GPIO.PWM(enb,1000)
+p1.start(25)
+p2.start(25)
+
+def forward(distance=1):
+    adjust_speed(low)
+    GPIO.output(in1,GPIO.HIGH)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.HIGH)
+    GPIO.output(in4,GPIO.LOW)
+    time.sleep(1*distance)
+    stop_motors()
+
+def backward(distance=1):
+    adjust_speed(low)
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.HIGH)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.HIGH)
+    time.sleep(1*distance)
+    stop_motors()
+
+def rotate(degrees=1):
+    adjust_speed(high)
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.HIGH)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.HIGH)
+    time.sleep(1*degrees)
+    stop_motors()
+
+def adjust_speed(level):
+    if level == 'low':
+        p1.ChangeDutyCycle(10)
+        p2.ChangeDutyCycle(10)
+    elif level == 'medium':
+        p1.ChangeDutyCycle(20)
+        p2.ChangeDutyCycle(20)
+    elif level == 'high':
+        p1.ChangeDutyCycle(35)
+        p2.ChangeDutyCycle(35)
+    else:
+        print("Invalid speed level")
+
+def stop_motors():
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.LOW)
 
 if __name__ == "__main__":
 
