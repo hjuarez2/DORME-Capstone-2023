ed1a7bb801bb4efebda9932f44e3a40099dbcbd9
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-03 16:34:42 -0500

    Update simpleCompassTest2.py

--- Testing_Sandbox/simpleCompassTest2.py
+++ Testing_Sandbox/simpleCompassTest2.py
@@ -6,7 +6,6 @@
 #import draft_pathfind.py as pf
 import RPi.GPIO as GPIO          
 from time import sleep
-from pathfind import short_path
 from Testing_Sandbox.draft_connect import from_name_to_coordinates
 from Testing_Sandbox.draft_connect import from_coordinates_to_distance
 import board
