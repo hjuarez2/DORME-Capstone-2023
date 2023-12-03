828c36653d34283cd6bad6c55af7b79253c372c0
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-03 15:37:57 -0500

    Update simpleCompassTest.py

--- Testing_Sandbox/simpleCompassTest.py
+++ Testing_Sandbox/simpleCompassTest.py
@@ -8,49 +8,35 @@ i2c = board.I2C() # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C() # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_lis3mdl.LIS3MDL(i2c)
 
-compass_range = 360
-    
-    # Adjust the readings to fall within the range of 0 to 360 degrees
-    
-
-def magnetometer_to_compass(readout):
-    # Define the range of values
-    north_reading = 30.916822566500954
-    south_reading = -2.943678748903833
-    
-    # Map the readings to compass degrees
-    compass_range = north_reading - south_reading
-    degrees = ((readout - south_reading) / compass_range) * 360
-    
-    # Normalize the degrees to [0, 360)
-    normalized_degrees = degrees % 360
-    
-    return normalized_degrees
-
-
 count = 0
 total=0
+
+
+def magnetometer_to_compass_degree(reading):
+    if reading >= 0:
+        return 180 - (reading * (180 / 30.916822566500954))
+    else:
+        return abs(reading) * (180 / 2.943678748903833)
+
+
+
+
 while True:
  
  
  mag_x, mag_y, mag_z = sensor.magnetic
  #print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f} uT".format(mag_x, mag_y, mag_z))
  #print(mag_x)
- #count+=1
- #total+=mag_x
+ count+=1
+ total+=mag_x
  #print("count: " + str(count)+ " avg: "+ str(total/count))
- #compass_degrees = (mag_x % compass_range + compass_range) % compass_range
- print(magnetometer_to_compass(mag_x)
- #print(str(mag_x) + " " + str(compass_degrees))
+ print(magnetometer_to_compass_degree(mag_x))
  print("")
- time.sleep(0.5)
+ time.sleep(0.05)
 
 
 #after 2000 ticks averaged, north is 30.916822566500954
 #after 2000 ticks averaged, south is -2.943678748903833
-#total degrees: 67.721002630809574, factor of 5.3159283828473400526702265028795
-
-#to translate to degrees, -30.916822566500954 and times
 
 #clockwise subtracts
 #counterclockwise adds
