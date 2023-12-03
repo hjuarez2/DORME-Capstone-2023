fc8a733e9d229fe59b7456af0b722f45557b1cfc
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-03 14:58:09 -0500

    Update simpleCompassTest.py

--- Testing_Sandbox/simpleCompassTest.py
+++ Testing_Sandbox/simpleCompassTest.py
@@ -10,6 +10,6 @@ sensor = adafruit_lis3mdl.LIS3MDL(i2c)
 while True:
  mag_x, mag_y, mag_z = sensor.magnetic
  #print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f} uT".format(mag_x, mag_y, mag_z))
- print(mag_z)
+ print(mag_x)
  print("")
  time.sleep(0.5)
