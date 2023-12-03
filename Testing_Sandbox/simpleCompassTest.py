311e91770e05be6cfb92fb2ac8848110447f1302
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-03 15:01:07 -0500

    Update simpleCompassTest.py

--- Testing_Sandbox/simpleCompassTest.py
+++ Testing_Sandbox/simpleCompassTest.py
@@ -7,9 +7,15 @@ import adafruit_lis3mdl
 i2c = board.I2C() # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C() # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_lis3mdl.LIS3MDL(i2c)
+
+count = 0
+total=0
 while True:
+ count+=1
+ total+=mag_x
  mag_x, mag_y, mag_z = sensor.magnetic
  #print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f} uT".format(mag_x, mag_y, mag_z))
- print(mag_x)
+ #print(mag_x)
+ print(total/count)
  print("")
  time.sleep(0.5)
