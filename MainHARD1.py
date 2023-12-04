7c14523b69d155c82f7ee6e3a8c8a5d42b791584
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-03 19:26:12 -0500

    Update MainHARD1.py

--- MainHARD1.py
+++ MainHARD1.py
@@ -101,6 +101,7 @@ if __name__ == "__main__":
 
     # array of nodes -> draft connect function
     cartesian_coordinate_list = from_name_to_coordinates(node_name_list)
+    print(cartesian_coordinate_list)
     polar_coordinate_list = from_coordinates_to_distance(cartesian_coordinate_list)
 
     for polar_coordinate_pair in polar_coordinate_list:
