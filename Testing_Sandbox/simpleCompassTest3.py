9b26473e187e3be8191ecf7cc60529a7814f8bec
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-04 01:54:31 -0500

    Update simpleCompassTest3.py

--- Testing_Sandbox/simpleCompassTest3.py
+++ Testing_Sandbox/simpleCompassTest3.py
@@ -1,62 +1,28 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-""" Display magnetometer data once per second """
-import time
 import board
-import math
+import busio
 import adafruit_lis3mdl
-i2c = board.I2C() # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C() # For using the built-in STEMMA QT connector on a microcontroller
+import math
+
+# Create I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create sensor object
 sensor = adafruit_lis3mdl.LIS3MDL(i2c)
-import RPi.GPIO as GPIO    
 
+# Optionally adjust sensor settings
+sensor.range = adafruit_lis3mdl.RANGE_4_GAUSS  # Set the range to 4 Gauss
 
+# Function to calculate heading
+def calculate_heading():
+    mag_x, mag_y, _ = sensor.magnetic  # Read magnetic data
 
-def calculate_heading(x, y):
-    heading = math.atan2(x, y)
-    # Convert radians to degrees
-    heading_degrees = math.degrees(heading)
-    # Normalize heading to be between 0 and 360 degrees
-    normalized_heading = (heading_degrees + 360) % 360
-    return normalized_heading
+    # Calculate heading
+    heading = math.atan2(mag_y, mag_x) * 180 / math.pi
+    if heading < 0:
+        heading += 360
 
-
+    return heading
 
 while True:
- 
- 
- mag_x, mag_y, mag_z = sensor.magnetic
- 
-
- 
- #heading = math.atan2(mag_y, mag_x) * (180 / math.pi)
- #print("X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f} uT".format(mag_x, mag_y, mag_z))
- #print(mag_x)
- #readings=[]
- #count+=1
- #total+=mag_x
- #print("count: " + str(count)+ " avg: "+ str(total/count))
- #print(str(mag_x)+ "   " + str(magnetometer_to_compass_degree(mag_x)))
- #heading_rad = math.atan2(mag_y, mag_x)
- #heading_deg = math.degrees(heading_rad)
- #if heading_deg < 0:
-  #  heading_deg += 360
- #print(heading)
-
- # Replace these values with your magnetometer readings
- # magnetometer_x = 0.5
- # magnetometer_y = -0.866
- result_heading = calculate_heading(mag_x, mag_y)
- print("Heading:", result_heading)
- #count = 0
- #total=0
- time.sleep(0.5)
-
-#after 2000 ticks averaged, north is 30.916822566500954
-#after 2000 ticks averaged, south is -2.943678748903833
-#after 2000 ticks averaged, east is 10.669614989375537
-#after 2000 ticks averaged, west is 12.901322549049544
-
-
-#clockwise subtracts
-#counterclockwise adds
+    heading = calculate_heading()
+    print("Heading: {:.2f} degrees".format(heading))
