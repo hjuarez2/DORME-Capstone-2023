505c0ba1e1bd303cf0ae567a646e7a5d7ff1266a
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-05 14:14:54 -0500

    Update simpleCompassTest4.py

--- Testing_Sandbox/simpleCompassTest4.py
+++ Testing_Sandbox/simpleCompassTest4.py
@@ -49,9 +49,9 @@ p1.start(25)
 p2.start(25)
 
 sensor.range = Range.RANGE_4_GAUSS
-x_offfset= -15.35
-y_offset = -12.07
-z_offset = 31.74
+x_offfset= -6.18
+y_offset = -15.63
+z_offset = 38.71
 degree_offset = -326.8828
 
 total_list=0
