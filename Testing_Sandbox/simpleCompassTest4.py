4c9f274d87ec4208f05394e09e3096268891d762
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-04 23:18:09 -0500

    asdas

--- Testing_Sandbox/simpleCompassTest4.py
+++ Testing_Sandbox/simpleCompassTest4.py
@@ -7,21 +7,15 @@ from math import atan2, degrees
 import board
 import adafruit_lis3mdl
 from adafruit_lis3mdl import  Range
-import RPi.GPIO as GPIO          
-from time import sleep
-from pathfind import short_path
-import board
-
-
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_lis3mdl.LIS3MDL(i2c)
 
 sensor.range = Range.RANGE_4_GAUSS
-x_offfset= -12.40
-y_offset = -20.42
-z_offset = -9.49
+x_offfset= 12.40
+y_offset = 20.42
+z_offset = -29.49
 degree_offset = -326.8828
 
 total_list=0
@@ -40,31 +34,10 @@ def get_heading(_sensor):
     magnet_y +=y_offset
     return vector_2_degrees(magnet_x, magnet_y)
 
-def rotate(degrees):
-    adjust_speed(50, 50)
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(in2,GPIO.LOW)
-    GPIO.output(in3,GPIO.LOW)
-    GPIO.output(in4,GPIO.HIGH)
-    print("orienting to: "+ str(degrees))
-    while (abs(get_heading(sensor)-degrees) > 5):
-        continue
-    print(get_heading(sensor))
-    stop_motors()
-
-def adjust_speed(left, right):
-    global lSpeed
-    global rSpeed 
-    lSpeed = left
-    rSpeed = right
-    p1.ChangeDutyCycle(lSpeed)
-    p2.ChangeDutyCycle(rSpeed)
-
 
 while True:
    # total_list+=get_heading(sensor)
     #samples+=1
     #print(total_list/samples)
-    rotate(90)
     print("heading: {:.2f} degrees".format(get_heading(sensor)))
-    time.sleep(1)
+    time.sleep(0.5)
