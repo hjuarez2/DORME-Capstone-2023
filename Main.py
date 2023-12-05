710897a3b4dc20ee396618e4e69465e6b80c77ca
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-05 00:17:39 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -72,7 +72,7 @@ p2.start(25)
 
 def forward(distance = 1):
     global forwardStartHeading
-    forwardStartHeading = get_heading(_sensor)
+    forwardStartHeading = get_heading(sensor)
     adjust_speed(75, 75)
     GPIO.output(in1,GPIO.HIGH)
     GPIO.output(in2,GPIO.LOW)
