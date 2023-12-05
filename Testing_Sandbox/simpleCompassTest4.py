b0ae288d18ec48116ca96e80c2e7af1c0e51cc83
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-04 23:23:40 -0500

    Update simpleCompassTest4.py

--- Testing_Sandbox/simpleCompassTest4.py
+++ Testing_Sandbox/simpleCompassTest4.py
@@ -2,11 +2,15 @@
 # SPDX-License-Identifier: MIT
 
 """ Display compass heading data five times per second """
-import time
+
 from math import atan2, degrees
 import board
 import adafruit_lis3mdl
 from adafruit_lis3mdl import  Range
+import RPi.GPIO as GPIO          
+from time import sleep
+from pathfind import short_path
+import board
 
 
 
@@ -14,14 +18,63 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_lis3mdl.LIS3MDL(i2c)
 
+# Pin setup and Constants
+in1 = 24
+in2 = 23
+in3 = 17
+in4 = 27
+ena = 12
+enb = 13
+
+lSpeed = 75
+rSpeed = 75
+forwardStartHeading = 0
+
+#GPIO initialization
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(ena,GPIO.OUT)
+GPIO.setup(enb,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+p1 = GPIO.PWM(ena,1000)
+p2 = GPIO.PWM(enb,1000)
+p1.start(25)
+p2.start(25)
+
 sensor.range = Range.RANGE_4_GAUSS
 x_offfset= -12.40
 y_offset = -20.42
-z_offset = -9.49
+z_offset = 29.49
 degree_offset = -326.8828
 
 total_list=0
 samples = 0
+
+def rotate(degrees):
+    adjust_speed(50, 50)
+    GPIO.output(in1,GPIO.HIGH)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.HIGH)
+    print("orienting to: "+ str(degrees))
+    while (abs(get_heading(sensor)-degrees) > 5):
+        continue
+    print(get_heading(sensor))
+    stop_motors()
+
+def adjust_speed(left, right):
+    global lSpeed
+    global rSpeed 
+    lSpeed = left
+    rSpeed = right
+    p1.ChangeDutyCycle(lSpeed)
+    p2.ChangeDutyCycle(rSpeed)
 
 def vector_2_degrees(x, y):
     angle = degrees(atan2(y, x))
@@ -38,8 +91,12 @@ def get_heading(_sensor):
 
 
 while True:
+    rotate(90)
    # total_list+=get_heading(sensor)
     #samples+=1
     #print(total_list/samples)
     print("heading: {:.2f} degrees".format(get_heading(sensor)))
-    time.sleep(0.5)
+    time.sleep(1)
+
+    rotate(180)
+    time.sleep(1)
