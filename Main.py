d16d10d11008428804e9fc685d717ba71e306413
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-05 22:51:17 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -136,7 +136,6 @@ def rotate(degrees):
     GPIO.output(in3,GPIO.LOW)
     GPIO.output(in4,GPIO.HIGH)
     while (abs(get_heading(sensor)-degrees) > 1):
-        print(get_heading(sensor))
         continue
     print(get_heading(sensor))
     stop_motors()
@@ -205,7 +204,6 @@ if __name__ == "__main__":
     print("Delivery Successful!")
     for _ in range(4):
         continue
-    rotate(180)
-    rotate(360)
+    #unblock dance()
     GPIO.cleanup()
 
