0ed34d6e13720ab745cb58c9b2dd59e07d6097e9
NoorAchkar <nachkar@nd.edu>
2023-12-05 23:08:39 -0500

    multiplied by correct amount

--- Testing_Sandbox/draft_connect.py
+++ Testing_Sandbox/draft_connect.py
@@ -14,7 +14,10 @@ def from_name_to_coordinates(node_list):
 def from_coordinates_to_distance(new_list):
     del second_list[:]
     for i in range (0, len(new_list)-1):
-        second_list.append(two_coordinates_to_distance_and_bearing(new_list[i], new_list[i+1]))
+        distance_and_bearing = two_coordinates_to_distance_and_bearing(new_list[i], new_list[i+1])
+        first_value = distance_and_bearing[0]
+        modified_tuple = (84922.48010 * first_value, distance_and_bearing[1])
+        second_list.append(modified_tuple)
     
     return second_list
 
@@ -22,8 +25,11 @@ if __name__ == "__main__":
     start_point = input("Enter the starting point: ")
     end_point = input("Enter the end point: ")
     node_list = short_path(start_point, end_point)
-    from_name_to_coordinates(node_list)
-    from_coordinates_to_distance(new_list)
+
+    # array of nodes -> draft connect function
+    new_list = from_name_to_coordinates(node_list)
+    second_list = from_coordinates_to_distance(new_list)
+
     print(f"Second list: {second_list}")
 
     
