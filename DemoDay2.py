058c325e44f759560b59271cdb5de7ff3a1b24c7
Hector Juarez <69487104+hjuarez2@users.noreply.github.com>
2023-12-14 08:15:02 -0500

    Update DemoDay2.py

--- DemoDay2.py
+++ DemoDay2.py
@@ -161,14 +161,8 @@ def stop_motors():
     GPIO.output(in4,GPIO.LOW)
 
 if __name__ == "__main__":
-    for i in range(5):
-        rotate(270)
-        sleep(1)
-        forward(1)
-        sleep(1)
-        rotate(90)
-        sleep(1)
-        forward(1)
+    while True:
+        print(get_heading(sensor))
         sleep(1)
     GPIO.cleanup()
 
